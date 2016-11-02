49d9815164ced6059c22947026777b088c4da922
Sebastian <sebastianheldt@outlook.de>
2016-11-02 22:53:08 +0100

    Changend Name of vehilce to correct vehicle

--- crayfireGUIsample/page/vehicleAddForm.xaml.cs
+++ crayfireGUIsample/page/vehicleAddForm.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace crayfireGUIsample.page
 {
     /// <summary>
-    /// Interaction logic for vehilceAddFormxaml.xaml
+    /// Interaction logic for vehilceAddForm.xaml
     /// </summary>
-    public partial class vehilceAddFormxaml : Page
+    public partial class vehicleAddForm : Page
     {
-        public vehilceAddFormxaml()
+        public vehicleAddForm()
         {
             InitializeComponent();
         }
